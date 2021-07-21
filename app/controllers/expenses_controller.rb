477ac994b1dfe04586f22e53bf9b370f15f9bc28
Kay Wallaschek <kayluis.wallaschek@stud.tu-darmstadt.de>
2021-07-21 22:16:50 +0900

    Add Expenses Show

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,15 +1,24 @@
 class ExpensesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_expenses, only: 'index'
+  before_action :set_expense, only: 'show'
 
 
   def index
 
   end
 
+  def show
+    
+  end
+
   private
 
   def set_expenses
     @expenses = Expense.where(household_id: current_user.main_household)
+  end
+
+  def set_expense
+    @expense = Expense.find(params[:id])
   end
 end
