d7cfc2220219ccb460091d1aa66bec85a058c888
Kay Wallaschek <kayluis.wallaschek@stud.tu-darmstadt.de>
2021-07-22 01:57:13 +0900

    Make expenses only accessable if the user is a member of it

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,6 +2,7 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_expenses, only: 'index'
   before_action :set_expense, only: 'show'
+  before_action :only_household_member, only: 'show'
 
 
   def index
@@ -9,10 +10,15 @@ class ExpensesController < ApplicationController
   end
 
   def show
-    
+
   end
 
   private
+
+  def only_household_member
+    return if current_user.households.include?(@expense.household)
+    redirect_to root_path, alert: "Not your Expense"
+  end
 
   def set_expenses
     @expenses = Expense.where(household_id: current_user.main_household)
